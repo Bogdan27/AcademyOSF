b526e90ff685baaa00d47d389c221e863049b418
DanEmilianBogdan <emilan.dan@student.usv.ro>
2019-11-14 22:48:56 +0200

    Added functionalities for RegisterNewCartRent with no validations

--- RentC/RentC/RentC/RegisterNewCartRentScreen.Designer.cs
+++ RentC/RentC/RentC/RegisterNewCartRentScreen.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelCartPlate = new System.Windows.Forms.Label();
+            this.labelClientId = new System.Windows.Forms.Label();
+            this.labelStartDate = new System.Windows.Forms.Label();
+            this.labelEndDate = new System.Windows.Forms.Label();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.textBoxCartPlate = new System.Windows.Forms.TextBox();
+            this.textBoxClientID = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelCartPlate
+            // 
+            this.labelCartPlate.AutoSize = true;
+            this.labelCartPlate.Font = new System.Drawing.Font("Arial", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCartPlate.Location = new System.Drawing.Point(40, 50);
+            this.labelCartPlate.Name = "labelCartPlate";
+            this.labelCartPlate.Size = new System.Drawing.Size(123, 28);
+            this.labelCartPlate.TabIndex = 0;
+            this.labelCartPlate.Text = "Cart Plate";
+            // 
+            // labelClientId
+            // 
+            this.labelClientId.AutoSize = true;
+            this.labelClientId.Font = new System.Drawing.Font("Arial", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClientId.Location = new System.Drawing.Point(40, 110);
+            this.labelClientId.Name = "labelClientId";
+            this.labelClientId.Size = new System.Drawing.Size(109, 28);
+            this.labelClientId.TabIndex = 1;
+            this.labelClientId.Text = "Client ID";
+            // 
+            // labelStartDate
+            // 
+            this.labelStartDate.AutoSize = true;
+            this.labelStartDate.Font = new System.Drawing.Font("Arial", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStartDate.Location = new System.Drawing.Point(39, 170);
+            this.labelStartDate.Name = "labelStartDate";
+            this.labelStartDate.Size = new System.Drawing.Size(124, 28);
+            this.labelStartDate.TabIndex = 2;
+            this.labelStartDate.Text = "Start Date";
+            // 
+            // labelEndDate
+            // 
+            this.labelEndDate.AutoSize = true;
+            this.labelEndDate.Font = new System.Drawing.Font("Arial", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEndDate.Location = new System.Drawing.Point(40, 230);
+            this.labelEndDate.Name = "labelEndDate";
+            this.labelEndDate.Size = new System.Drawing.Size(116, 28);
+            this.labelEndDate.TabIndex = 3;
+            this.labelEndDate.Text = "End Date";
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.Font = new System.Drawing.Font("Arial", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCity.Location = new System.Drawing.Point(40, 290);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(57, 28);
+            this.labelCity.TabIndex = 4;
+            this.labelCity.Text = "City";
+            // 
+            // textBoxCartPlate
+            // 
+            this.textBoxCartPlate.Location = new System.Drawing.Point(210, 50);
+            this.textBoxCartPlate.Name = "textBoxCartPlate";
+            this.textBoxCartPlate.Size = new System.Drawing.Size(200, 22);
+            this.textBoxCartPlate.TabIndex = 5;
+            // 
+            // textBoxClientID
+            // 
+            this.textBoxClientID.Location = new System.Drawing.Point(210, 110);
+            this.textBoxClientID.Name = "textBoxClientID";
+            this.textBoxClientID.Size = new System.Drawing.Size(200, 22);
+            this.textBoxClientID.TabIndex = 6;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(210, 290);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(200, 22);
+            this.textBoxCity.TabIndex = 9;
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(210, 230);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerStartDate.TabIndex = 10;
+            // 
+            // dateTimePickerEndDate
+            // 
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(210, 170);
+            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
+            this.dateTimePickerEndDate.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerEndDate.TabIndex = 11;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(175, 380);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(159, 31);
+            this.buttonRegister.TabIndex = 12;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // RegisterNewCartRentScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.dateTimePickerEndDate);
+            this.Controls.Add(this.dateTimePickerStartDate);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxClientID);
+            this.Controls.Add(this.textBoxCartPlate);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.labelEndDate);
+            this.Controls.Add(this.labelStartDate);
+            this.Controls.Add(this.labelClientId);
+            this.Controls.Add(this.labelCartPlate);
+            this.Name = "RegisterNewCartRentScreen";
             this.Text = "RegisterNewCartRentScreen";
+            this.Load += new System.EventHandler(this.RegisterNewCartRentScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelCartPlate;
+        private System.Windows.Forms.Label labelClientId;
+        private System.Windows.Forms.Label labelStartDate;
+        private System.Windows.Forms.Label labelEndDate;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.TextBox textBoxCartPlate;
+        private System.Windows.Forms.TextBox textBoxClientID;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEndDate;
+        private System.Windows.Forms.Button buttonRegister;
     }
 }
--- RentC/RentC/RentC/RegisterNewCartRentScreen.cs
+++ RentC/RentC/RentC/RegisterNewCartRentScreen.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,79 @@ namespace RentC
 {
     public partial class RegisterNewCartRentScreen : Form
     {
+        String connectionString = ConfigurationManager.ConnectionStrings["rentConnectionString"].ConnectionString;
+
+
         public RegisterNewCartRentScreen()
         {
             InitializeComponent();
         }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            addReservation();
+          //  string s=
+               
+        }
+
+        private void RegisterNewCartRentScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addReservation()
+        {
+                try
+                {
+
+                    using (SqlConnection openCon = new SqlConnection(connectionString))
+                    {
+                        string saveStaff = "INSERT into Reservations (carId,costumerID,ReservStatsID,startDate,endDate,location) VALUES (@carId,@costumerId,@ReservStatsID,@startDate,@endDate,@location)";
+
+                        using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                        {
+                            querySaveStaff.Connection = openCon;
+                            querySaveStaff.Parameters.Add("@carId", SqlDbType.Int).Value = Int32.Parse(textBoxCartPlate.Text);
+                            querySaveStaff.Parameters.Add("@costumerId", SqlDbType.Int).Value = Int32.Parse(textBoxClientID.Text);
+                            querySaveStaff.Parameters.Add("@ReservStatsID", SqlDbType.Int).Value = 1;
+                            querySaveStaff.Parameters.Add("@startDate", SqlDbType.Date, 30).Value = dateTimePickerStartDate.Value.ToString("dd/MMM/yyyy");
+                            querySaveStaff.Parameters.Add("@endDate", SqlDbType.Date, 30).Value = dateTimePickerEndDate.Value.ToString("dd/MMM/yyyy");
+                            querySaveStaff.Parameters.Add("@location", SqlDbType.VarChar, 50).Value = textBoxCity.Text;
+
+
+
+                            openCon.Open();
+
+                            int result = querySaveStaff.ExecuteNonQuery();
+
+                            if (result > 0)
+                            {
+                                MessageBox.Show("Adaugat");
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Eroare");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exceptie" + ex.Message);
+                }
+            
+        }
+
+        private bool ValidateDate()
+        {
+            bool ok = true;
+            //if ()
+            //{
+
+            //}
+            return ok;
+        }
+
     }
 }
